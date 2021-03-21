40511ccd4b2458dec093440adffdbea6b5f61a9e
alpkang <ahmetalpkan@gmail.com>
2021-03-21 14:50:48 +0300

    db update

--- crt_ast/models.py
+++ crt_ast/models.py
@@ -5,79 +5,77 @@ from django.contrib.gis.db import models
 
 
 class AssetType(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     industry = models.CharField(max_length=30, null=True, blank=True)
-    description = models.CharField(max_length=256, null=True)
-
-    def save(self, *args, **kwargs):
-        if not self.industry:
-            self.industry = None
-        super(AssetType, self).save(*args, **kwargs)
+    description = models.CharField(max_length=256, null=True, blank=True)
 
     def __str__(self, *args, **kwargs):
     	return self.name
 
-
 class AssetTypeProperty(models.Model):
     type = models.ForeignKey(AssetType, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     dtype = models.CharField(max_length=30, null=True)
-    description = models.CharField(max_length=256, null=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
 
+    def __str__(self, *args, **kwargs):
+    	return "{}, Type: {}".format(self.name, self.type.name)
 
 class AssetTypeSymbol(models.Model):
     type = models.ForeignKey(AssetType, on_delete=models.CASCADE)
-    symbol = models.ImageField(upload_to='symbol_img/')
-    description = models.CharField(max_length=256, null=True)
+    symbol = models.ImageField(upload_to='symbol_img/', unique=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
+
+    def __str__(self, *args, **kwargs):
+    	return "{}, Type: {}".format(str(self.symbol), self.type.name)
 
 class Office(models.Model):
     department = models.CharField(max_length=75)
-    office_location = models.CharField(max_length=100, null=True)
-    email = models.EmailField(max_length=100, null=True)
-    description = models.CharField(max_length=256, null=True)
+    office_location = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
 
-
-class SeniorStaff(models.Model):
-    office_info = models.ForeignKey(Office, on_delete=models.CASCADE)
-    name = models.CharField(max_length=75)
-    surname = models.CharField(max_length=75)
-    title = models.CharField(max_length=75)
-    email = models.EmailField(max_length=100)
-    phone = models.IntegerField()
-
+    def __str__(self, *args, **kwargs):
+    	return "{} {}".format(self.department, self.office_location)
 
 class Staff(models.Model):
     office_info = models.ForeignKey(Office, on_delete=models.CASCADE)
     name = models.CharField(max_length=75)
     surname = models.CharField(max_length=75)
-    title = models.CharField(max_length=75)
-    email = models.EmailField(max_length=100)
-    phone = models.IntegerField()
-    reports_to = models.ForeignKey(SeniorStaff, on_delete=models.CASCADE)
+    title = models.CharField(max_length=75, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True, unique=True)
+    phone = models.IntegerField(null=True, blank=True, unique=True)
+    #reports_to = models.ForeignKey(SeniorStaff, on_delete=models.CASCADE)
 
+    def __str__(self, *args, **kwargs):
+    	return "{} {}, Job Title: {}".format(self.name, self.surname, self.title)
 
 class Asset(models.Model):
     type = models.ForeignKey(AssetType, on_delete=models.CASCADE)
-    dataEntryStaff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     # BaseSpatialField.srid()
-    geom = models.GeometryField()
-    elevation = models.IntegerField(null=True)
-    photo = models.ImageField(null=True, upload_to='asset_photo/')
-    comissioning_date = models.DateField(null=True)
-    decommission_date = models.DateField(null=True)
+    geom = models.GeometryField(dim=True)
+    elevation = models.IntegerField(null=True, blank=True)
+    photo = models.ImageField(null=True, blank=True, upload_to='asset_photo/')
+    comissioning_date = models.DateField(null=True, blank=True)
+    decommission_date = models.DateField(null=True, blank=True)
 
     active_choices = (('Yes', "Active"), ('No', "Inactive"),
                       ('Maintenance', "Maintenance"))
     active = models.CharField(max_length=11,
-                              choices=active_choices)
+                              choices=active_choices, null=True, blank=True)
 
-    description = models.CharField(max_length=256, null=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
 
+    def __str__(self, *args, **kwargs):
+    	return "{}, Type: {}".format(self.name, self.type.name)
 
 class AssetPropertyValue(models.Model):
     property = models.ForeignKey(AssetTypeProperty, on_delete=models.CASCADE)
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
     value = models.CharField(max_length=256)
-    description = models.CharField(max_length=256, null=True)
+    description = models.CharField(max_length=256, null=True, blank=True)
+
+    def __str__(self, *args, **kwargs):
+    	return "Type: {}, Property: {}, Value: {}".format(self.asset.name, self.property.name, self.value)
+
